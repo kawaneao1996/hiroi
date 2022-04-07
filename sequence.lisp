f6cd0587e58dad3eb85b20e82ddc7eb136d66b1c
kawane1996 <matsuoyoshihiro0426@gmail.com>
2022-04-07 21:51:19 +0900

    retukansuu

---
+++ sequence.lisp
@@ -0,0 +1,10 @@
+;;sequence = list + string + vector
+;;elt sequence index
+(elt '(11 12 13 14 15) 0)
+;;subseq seq start [end]
+(subseq #(11 12 13 14 15) 1 4)
+;;copy-seq seq
+(copy-seq '(1 2 3 4)
+;;length seq
+;;reverse seq
+;;make-sequence type size
