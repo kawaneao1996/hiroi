c6549e627cb77960bf36b8e21a06509021d8b7d4
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2022-04-13 09:54:08 +0900

    0413

--- sequence.lisp
+++ sequence.lisp
@@ -44,3 +44,47 @@
 (remove 'b *vec*)
 *vec*
 ;;remove-if(-not) predicate sequence
+(defun mklst2 (s e)
+  (do ((i s (1+ i)) (rslt nil))
+      ((> i e) rslt)
+    (setq rslt (append rslt (list i)))))
+(defun mklst (s e)
+  (do ((i s (1+ i)) (rslt nil))
+      ((> i e) (reverse rslt))
+    (push i rslt)))
+(remove-if #'oddp (mklst 11 20))
+
+;;substitute new old sequence :oldと等しい要素をnewに置き換える
+(substitute 13 12 (mklst 1 20))
+
+;;substitute-if(-not) new predicate sequence : predicateを満たす要素を置き換え
+(substitute-if 0 #'oddp (mklst 1 20))
+
+;;fill sequence item : 列の要素をitemで置き換える
+(fill (mklst 1 20) pi :start 4 )
+(fill (mklst 1 20) 99 :start 4 :end 7)
+;;remove-duplicates sequence : 列の重複した要素を取り除く
+(remove-duplicates (fill (mklst 1 20) 99 :start 4 :end 7))
+
+(defun mkid (n element)
+  (let ((rslt nil))
+    (dotimes (i n rslt)
+      (push element rslt))))
+
+;; :count は処理する要素の個数を制限
+(remove 9  (mkid 20 9))
+(remove 9  (mkid 20 9) :count 3)
+(length (remove 9  (mkid 20 9) :count 3)) ; = 17
+(remove 9 (append (mklst 1 20) (mkid 10 9) (mklst 11 15)) :count 6)
+
+;;dot対のリストを作る
+(defun mkdots2 (lst1 lst2)
+  (if (and (null lst1) (null lst2))
+	nil
+	(cons (cons (car lst1) (car lst2))
+	      (mkdots2 (cdr lst1) (cdr lst2)))))
+	     
+(defun mkdots (lst1 lst2)
+  (let ((rslt nil) (count (min (length lst1) (length lst2))))
+    (dotimes (i count (reverse rslt))
+      (push (cons (nth i lst1) (nth i lst2)) rslt))))
